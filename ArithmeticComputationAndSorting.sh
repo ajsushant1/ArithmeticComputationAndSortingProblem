55c6085be65e734864716fe5cb72514519c340a1
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-11 19:15:36 +0530

    ADDED: Reading three numbers for Arithmetic Computation

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -x
 
 echo "/*********************************** ARITHMETIC COMPUTATION AND SORTING ***********************************/"
+
+# READING THREE NUMBER INPUT 
+read -p "Enter the first number :" a
+read -p "Enter the Second number :" b
+read -p "Enter the third number :" c
+
+
