6999d6e10fde1c7b1e7110ace0115d0a20fb73b6
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-14 09:58:23 +0530

    ADDED: Reading values from the dictionary into the array

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -2,6 +2,9 @@
 
 echo "/*********************************** ARITHMETIC COMPUTATION AND SORTING ***********************************/"
 
+# VARIABLE
+dictionaryLength=0
+
 # DECLARING DICTIONARY
 declare -A resultDictionary
 
@@ -10,10 +13,17 @@ read -p "Enter the first number :" a
 read -p "Enter the Second number :" b
 read -p "Enter the third number :" c
 
-# COMPUTING ARITHMETIC OPERATION AND STORING IN DICTIONARY
+# COMPUTING ARITHMETIC OPERATION AND STORING THE REASULT IN DICTIONARY
 resultDictionary[operation1]=$((a+b*c))
 resultDictionary[operation2]=$((a*b+c))
 resultDictionary[operation3]=`echo "scale=2; $c+$a/$b" | bc`
 resultDictionary[operation4]=`echo "$a%$b+$c" | bc`
 
+# COMPUTING DICTIONARY LENGTH
+dictionaryLength=${#resultDictionary[@]}
 
+# READING VALUES FROM THE DICTIONARY INTO THE ARRAY
+for (( index=1; index<=dictionaryLength; index++ ))
+do
+	resultArray[index]=${resultDictionary[operation$((index))]}
+done
