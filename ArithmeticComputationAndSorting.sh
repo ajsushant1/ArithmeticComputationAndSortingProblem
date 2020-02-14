75e1951d0412d2381f1fcd29325b1025d7def5d3
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-14 09:27:11 +0530

    ADDED: Storing result of arithmetic operation in dictionary

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -2,13 +2,18 @@
 
 echo "/*********************************** ARITHMETIC COMPUTATION AND SORTING ***********************************/"
 
-# READING THREE NUMBER INPUT 
+# DECLARING DICTIONARY
+declare -A resultDictionary
+
+# READING THREE NUMBER INPUT
 read -p "Enter the first number :" a
 read -p "Enter the Second number :" b
 read -p "Enter the third number :" c
 
-# ARITHMETIC COMPUTATIONS
-firstOperation=$((a+b*c))
-secondOperation=$((a*b+c))
-thirdOperartion=`echo "scale=2; $c+$a/$b" | bc`
-forthOperation=`echo "$a%$b+$c" | bc`
+# COMPUTING ARITHMETIC OPERATION AND STORING IN DICTIONARY
+resultDictionary[operation1]=$((a+b*c))
+resultDictionary[operation2]=$((a*b+c))
+resultDictionary[operation3]=`echo "scale=2; $c+$a/$b" | bc`
+resultDictionary[operation4]=`echo "$a%$b+$c" | bc`
+
+
